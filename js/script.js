4bc34606bab3c8de66d79877b44fb7a12c55f642
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-11-07 14:13:54 +0100

    feat: const lamp and btn

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 // Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+//Costante della lampada
+const whiteLamp = document.getElementById("white-lamp");
+console.log(whiteLamp)
+
+//Costante del bottone
+const btn = document.getElementById("lamp-btn");
+console.log(btn)
+
+
+
+
 // **BONUS**
 // Facciamo accendi e spegni:
 // - Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
